18dcbc8c623d70615b2fce34efc274832158c4bd
Bria White <bria.awhite@gmail.com>
2020-08-08 13:20:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,8 +52,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash.each do |seaon, holiday_party|
-   puts "#{holiday_party}"
+  
  end
 end
 
